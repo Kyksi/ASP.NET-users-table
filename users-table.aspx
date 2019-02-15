d33edd21aa98c75aa0bb2116c387de2dd83cf153
Kyksi <47677914+Kyksi@users.noreply.github.com>
2019-02-15 23:32:07 +0100

    Add files via upload

---
+++ users-table.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="users-table.aspx.cs" Inherits="ASP.NET_pages.users_table" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=0.6"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"/>
+    <title>Users</title>
+    <style>
+        table{
+            width: 94%;
+            margin: 10px 3% 10px 3%;
+            text-align: center;
+        }
+        input[type='text']{
+            width: 250px;
+            height: 30px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+            padding-left: 10px;
+        }
+        tr:hover{
+            background-color: lightgray;
+            transition: 1s;
+        }
+        input[type='button'], input[type='submit']{
+            width: 80px;
+            height: 40px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <a style="position: fixed; right: 29px;" href="https://nsemeniuk.000webhostapp.com/">Main page</a><br />
+            <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" 
+                BorderWidth="1px" CellPadding="4" ForeColor="Black" GridLines="Horizontal" 
+                 DataKeyNames="Id" 
+                AutoGenerateColumns="False" 
+                OnRowDeleting="GridView1_RowDeleting" OnRowCancelingEdit="GridView1_RowCancelingEdit" 
+                OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" EnableModelValidation="False" ShowFooter="True">
+                <Columns>
+                    <asp:BoundField DataField="Id" HeaderText="Id" Visible="False" />
+                    <asp:BoundField DataField="name" HeaderText="Name" />
+                    <asp:BoundField HeaderText="Surname" DataField="surname" />
+                    <asp:BoundField HeaderText="Location" DataField="city" />
+                    <asp:CommandField ButtonType="Button" ShowEditButton="True" ControlStyle-CssClass="btn btn-dark">
+                        <ControlStyle CssClass="btn btn-dark"></ControlStyle>
+                    </asp:CommandField>
+                    <asp:CommandField ButtonType="Button" ShowDeleteButton="True" ControlStyle-CssClass="btn btn-danger">
+                        <ControlStyle CssClass="btn btn-danger"></ControlStyle>
+                    </asp:CommandField>
+                </Columns>
+                <FooterStyle BackColor="LightGray" ForeColor="Black" />
+                <HeaderStyle BackColor="#333333" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="White" ForeColor="Black" HorizontalAlign="Right" />
+                <SelectedRowStyle BackColor="#CC3333" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F7F7F7" />
+                <SortedAscendingHeaderStyle BackColor="#4B4B4B" />
+                <SortedDescendingCellStyle BackColor="#E5E5E5" />
+                <SortedDescendingHeaderStyle BackColor="#242121" />
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ users-table.aspx.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace ASP.NET_pages
+{
+    public partial class users_table : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                connect();
+            }
+            else
+            {
+                DodajDoStopki();
+            }
+        }
+
+        public void connect()
+        {
+            String connectionString = @"Data Source=SQL6004.site4now.net;Initial Catalog=DB_A4475E_database1;User Id=DB_A4475E_database1_admin;Password=Root_2000;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "Select * from users";
+            command.CommandType = CommandType.Text;
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+
+           DodajDoStopki();
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+
+            String id = GridView1.DataKeys[e.RowIndex]["Id"].ToString();
+            String connectionString = @"Data Source=SQL6004.site4now.net;Initial Catalog=DB_A4475E_database1;User Id=DB_A4475E_database1_admin;Password=Root_2000;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = String.Format("delete from users where id={0}", id);
+            command.CommandType = CommandType.Text;
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            connect();
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            connect();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            connect();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            String id = GridView1.DataKeys[e.RowIndex]["Id"].ToString();
+            String connectionString = @"Data Source=SQL6004.site4now.net;Initial Catalog=DB_A4475E_database1;User Id=DB_A4475E_database1_admin;Password=Root_2000;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            for (int i = 0; i < GridView1.Columns.Count; i++)
+            {
+                DataControlFieldCell objCell = (DataControlFieldCell)GridView1.Rows[e.RowIndex].Cells[i];
+                GridView1.Columns[i].ExtractValuesFromCell(e.NewValues, objCell, DataControlRowState.Edit, false);
+            }
+            command.CommandText = string.Format("update users set name='{0}',surname='{1}', city='{2}' where ID={3}", e.NewValues["name"], e.NewValues["surname"], e.NewValues["city"], id);
+            command.CommandType = CommandType.Text;
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            GridView1.EditIndex = -1;
+
+            connect();
+        }
+
+        public void DodajDoStopki()
+        {
+            
+            TextBox txt = new TextBox();
+            TextBox txt2 = new TextBox();
+            TextBox txt3 = new TextBox();
+            Button btn = new Button();
+            btn.ID = new Guid().ToString();
+            btn.CssClass = "btn btn-success";
+            btn.Text = "Add +";
+            btn.Click += new EventHandler(AddNew);
+            GridView1.FooterRow.Cells[1].Controls.Add(txt);
+            GridView1.FooterRow.Cells[2].Controls.Add(txt2);
+            GridView1.FooterRow.Cells[3].Controls.Add(txt3);
+            GridView1.FooterRow.Cells[5].Controls.Add(btn);
+            
+        }
+
+        public void AddNew(object sender, EventArgs e)
+        {
+            String connectionString = @"Data Source=SQL6004.site4now.net;Initial Catalog=DB_A4475E_database1;User Id=DB_A4475E_database1_admin;Password=Root_2000;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            String name = ((TextBox)GridView1.FooterRow.Cells[1].Controls[0]).Text;
+            String surname = ((TextBox)GridView1.FooterRow.Cells[2].Controls[0]).Text;
+            String city = ((TextBox)GridView1.FooterRow.Cells[3].Controls[0]).Text;
+            command.CommandText = String.Format("insert into users(name, surname, city) values('{0}','{1}', '{2}')", name, surname, city);
+            command.CommandType = CommandType.Text;
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            connect();
+        }
+    }
+}
